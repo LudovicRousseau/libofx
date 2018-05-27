ba7dcd269080b0e3814e4ca141d43a1f4795e052
xfred81 <fred@linuxtribe.org>
2018-05-27 14:35:30 +0200

    Added gengetopt

--- msys2/appveyor-build-ofx.sh
+++ msys2/appveyor-build-ofx.sh
@@ -1,13 +1,15 @@
 #! /bin/sh
 
+source /appveyor.environment
+export MYSTEM
+
+cp gengetopt_$MSYSTEM.exe /usr/bin
+
 export PATH=$PATH:/inst/bin
 cd ..
 ./autogen.sh
 ./configure --with-opensp-includes=/inst/include/OpenSP --with-opensp-libs=/inst/ --prefix=/inst
-make
+make -j 2
 make install
-
-source /appveyor.environment
-export MSYSTEM
 
 zip -9r /c/libofx_$MSYSTEM.zip /inst
