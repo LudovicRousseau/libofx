e0baa56b38fa9c5dcc06e7c9a0736a56f8eadf65
xfred81 <fred@linuxtribe.org>
2018-05-27 12:55:41 +0200

    Small improvements

--- msys2/appveyor-build-opensp.sh
+++ msys2/appveyor-build-opensp.sh
@@ -5,14 +5,19 @@ open_sp_version=1.5.2
 wget -O /OpenSP-$open_sp_version.tar.gz "https://sourceforge.net/projects/openjade/files/opensp/1.5.2/OpenSP-$open_sp_version.tar.gz/download"
 
 mpwd=`pwd`
+
+echo "Extracting..."
 cd /
-tar xzvf OpenSP-$open_sp_version.tar.gz
+tar xzvf OpenSP-$open_sp_version.tar.gz > /dev/null
 cd $mpwd
 
+echo "Patching..."
 cp opensp-$open_sp_version-msys2.patch.gz /OpenSP-$open_sp_version/
 cd /OpenSP-$open_sp_version
 gunzip opensp-$open_sp_version-msys2.patch.gz
 patch -s -p1 < opensp-1.5.2-msys2.patch
+
+echo "Initialise..."
 ./autoinit.sh
 ./configure --prefix=/inst --disable-doc-build
-make && make install
+make -j 2 && make install
