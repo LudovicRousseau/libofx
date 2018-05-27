f885ae9bab25221c1a1d9200fa9e79d29ab42ebb
xfred81 <fred@linuxtribe.org>
2018-05-27 14:23:34 +0200

    autogen.sh launched

--- msys2/appveyor-build-ofx.sh
+++ msys2/appveyor-build-ofx.sh
@@ -2,6 +2,7 @@
 
 export PATH=$PATH:/inst/bin
 cd ..
+./autogen.sh
 ./configure --with-opensp-includes=/inst/include/OpenSP --with-opensp-libs=/inst/ --prefix=/inst
 make
 make install
@@ -9,4 +10,4 @@ make install
 source /appveyor.environment
 export MSYSTEM
 
-zip -9r /libofx_$MSYSTEM.zip /inst
+zip -9r /c/libofx_$MSYSTEM.zip /inst
