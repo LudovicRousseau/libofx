9666084565aa721a9d760c09647ac5d04fb01548
Frédéric Trouche <32123746+xfred81@users.noreply.github.com>
2018-12-26 14:01:39 +0100

    Installation directory changed to /tmp/inst
    More portable default prefix.

--- msys2/appveyor-build-ofx.sh
+++ msys2/appveyor-build-ofx.sh
@@ -8,8 +8,8 @@ cp gengetopt_$MSYSTEM.exe /usr/bin/gengetopt.exe
 export PATH=$PATH:/inst/bin
 cd ..
 ./autogen.sh
-./configure --with-opensp-includes=/inst/include/OpenSP --with-opensp-libs=/inst/ --prefix=/inst
+./configure --with-opensp-includes=/inst/include/OpenSP --with-opensp-libs=/inst/ --prefix=/tmp/inst
 make -j 2
 make install
 
-zip -9r libofx_$MSYSTEM.zip /inst
+zip -9r libofx_$MSYSTEM.zip /tmp/inst
